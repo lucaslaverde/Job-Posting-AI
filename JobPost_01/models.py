c21e27ee3aa855979f1c249531630147cfc1ee60
Cirkandia <jemesav@eafit.edu.co>
2024-09-26 14:02:26 -0500

    base de datos inicial

--- JobPost_01/models.py
+++ JobPost_01/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    Usuario = models.CharField(unique = True, max_length=50)
+    Nombre = models.CharField(unique=False,max_length=25)
+    Email = models.CharField(unique=True,max_length=25)
+    Tpassw = models.CharField(unique=True,max_length=500)
+
+class IACONTENT(models.Model):
+    Empresa = models.CharField(unique=True, max_length=50)
+    COLOR_paletas = models.CharField(unique=False, max_length=100)
+    #plantillas = models.ForeignObject()
